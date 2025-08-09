ab29a4b59c776db34a962b3199b9ac581c9f95d6
Soares Chen <soares.chen@maybevoid.com>
2025-08-09 17:52:05 +0000

    Finish draft implementation of parallel shortest path

--- src/parallel_shortest_path.rs
+++ src/parallel_shortest_path.rs
@@ -1,5 +1,5 @@
 use rayon::prelude::*;
-use std::collections::{BTreeMap, BTreeSet};
+use std::collections::{BTreeMap, BTreeSet, VecDeque};
 
 use crate::traits::Graph;
 
@@ -8,8 +8,6 @@ pub struct State<G: Graph> {
     pub delta: G::Cost,
     pub costs: BTreeMap<G::Node, G::Cost>,
     pub predecessors: BTreeMap<G::Node, G::Node>,
-    pub current_bucket_id: usize,
-    pub current_bucket: BTreeSet<G::Node>,
     pub buckets: BTreeMap<usize, BTreeSet<G::Node>>,
 }
 
@@ -26,20 +24,104 @@ pub struct BucketResult<G: Graph> {
 }
 
 impl<G: Graph> State<G> {
-    pub fn process_bucket(&mut self, bucket: BTreeSet<G::Node>) {
-        let results = bucket
-            .into_par_iter()
-            .map(|node| self.process_neighbors(&node))
-            .collect_vec_list();
+    pub fn shortest_distance(
+        graph: G,
+        source: G::Node,
+        target: G::Node,
+    ) -> Option<(VecDeque<G::Node>, G::Cost)> {
+        let mut state = Self {
+            graph,
+            delta: G::Cost::default(),
+            costs: BTreeMap::new(),
+            predecessors: BTreeMap::new(),
+            buckets: BTreeMap::new(),
+        };
 
-        for result in results.into_iter().flatten().flatten() {
-            for neighbor in result.same_bucket_neighbors {
-                self.update_neighbor_of_node(&neighbor);
+        state.process_next_bucket(0, BTreeSet::from([source]));
+        state.process_buckets(target);
+        state.retrieve_result(source, target)
+    }
+
+    fn retrieve_result(
+        &mut self,
+        source: G::Node,
+        target: G::Node,
+    ) -> Option<(VecDeque<G::Node>, G::Cost)> {
+        let &cost = self.costs.get(&target)?;
+        let mut path = VecDeque::new();
+        while let Some(&predecessor) = self.predecessors.get(&target) {
+            path.push_front(predecessor);
+        }
+
+        debug_assert!(path.front().cloned() == Some(source));
+
+        Some((path, cost))
+    }
+
+    fn process_buckets(&mut self, target: G::Node) {
+        while let Some((bucket_id, bucket)) = self.buckets.pop_first() {
+            self.process_next_bucket(bucket_id, bucket);
+            if self.costs.contains_key(&target) {
+                return;
             }
         }
     }
 
-    pub fn update_neighbor(&mut self, neighbor: &Neighbor<G>) -> bool {
+    fn process_next_bucket(&mut self, bucket_id: usize, mut bucket: BTreeSet<G::Node>) {
+        loop {
+            bucket = self.process_bucket(bucket_id, bucket);
+            if bucket.is_empty() {
+                break;
+            }
+        }
+    }
+
+    fn process_bucket(
+        &mut self,
+        current_bucket_id: usize,
+        current_bucket: BTreeSet<G::Node>,
+    ) -> BTreeSet<G::Node> {
+        let results = current_bucket
+            .into_par_iter()
+            .map(|node| self.process_neighbors(&node))
+            .collect_vec_list();
+
+        let mut pending_bucket = BTreeSet::new();
+
+        for result in results.into_iter().flatten().flatten() {
+            for neighbor in result.same_bucket_neighbors {
+                self.update_neighbor(&mut pending_bucket, current_bucket_id, &neighbor);
+            }
+        }
+
+        pending_bucket
+    }
+
+    fn update_neighbor(
+        &mut self,
+        pending_bucket: &mut BTreeSet<G::Node>,
+        current_bucket_id: usize,
+        neighbor: &Neighbor<G>,
+    ) {
+        let new_cost = self.update_neighbor_cost(neighbor);
+        if new_cost {
+            self.predecessors
+                .insert(neighbor.node, neighbor.predecessor);
+
+            let bucket_id = G::floor_cost(neighbor.total_cost / self.delta);
+
+            debug_assert!(bucket_id >= current_bucket_id);
+
+            if bucket_id == current_bucket_id {
+                pending_bucket.insert(neighbor.node);
+            } else {
+                let bucket = self.buckets.entry(bucket_id).or_default();
+                bucket.insert(neighbor.node);
+            }
+        }
+    }
+
+    fn update_neighbor_cost(&mut self, neighbor: &Neighbor<G>) -> bool {
         if let Some(current_cost) = self.costs.get_mut(&neighbor.node) {
             if neighbor.total_cost < *current_cost {
                 *current_cost = neighbor.total_cost;
@@ -53,23 +135,7 @@ impl<G: Graph> State<G> {
         }
     }
 
-    pub fn update_neighbor_of_node(&mut self, neighbor: &Neighbor<G>) {
-        let new_cost = self.update_neighbor(neighbor);
-        if new_cost {
-            self.predecessors
-                .insert(neighbor.node, neighbor.predecessor);
-            let bucket_id = G::floor_cost(neighbor.total_cost / self.delta);
-
-            if bucket_id == self.current_bucket_id {
-                self.current_bucket.insert(neighbor.node);
-            } else {
-                let bucket = self.buckets.entry(bucket_id).or_default();
-                bucket.insert(neighbor.node);
-            }
-        }
-    }
-
-    pub fn process_neighbors(&self, node: &G::Node) -> Option<BucketResult<G>> {
+    fn process_neighbors(&self, node: &G::Node) -> Option<BucketResult<G>> {
         let current_cost = self.costs.get(node).cloned().unwrap_or_default();
 
         if let Some(neighbors) = self.graph.get_neighbors(node) {
